510c24016358fa0fdd1fa1d1294df278dc50751b
KyawZinLin3 <kyawzinlin14future@gmail.com>
2024-06-16 22:47:31 +0630

    Update:Basic Function

---
+++ ConsoleApp.TaskManagementApp/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.TaskManagementApp
+{
+    public class Task
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+
+        public Task(string title,string description)
+        {
+            Title = title;
+            Description = description;
+            IsComplete = false;
+        }
+
+        public string ToFileFormat()
+        {
+            return $"{Title}|{Description}|{IsComplete}";
+        }
+    }
+}
